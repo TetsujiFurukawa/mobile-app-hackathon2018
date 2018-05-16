3ae51377f0a51f9e67491cd263183e9e0c0eefd3
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2018-05-16 23:12:25 +0900

    作成中

--- 03.web-server/mobile-app-hackathon-server/src/main/java/jp/co/yahoo/tetsufuru1968/hackathon/repository/WorkRepository.java
+++ 03.web-server/mobile-app-hackathon-server/src/main/java/jp/co/yahoo/tetsufuru1968/hackathon/repository/WorkRepository.java
@@ -10,7 +10,9 @@ import jp.co.yahoo.tetsufuru1968.hackathon.dto.WorkDto;
 
 public interface WorkRepository extends JpaRepository<WorkDto, Integer> {
 	@Query(value = "SELECT wok.work_id, wok.pay_date, wok.user_id, wok.work_item_id, wok.child_comment, "
-			+ "wok.attached_image, wok.parent_comment_id, wok.approval , item.name as work_item_name, item.currency_id,item.number,null as approval_message "
-			+ "FROM spajam.t_work wok INNER JOIN spajam.m_work_item item ON item.work_item_id = wok.work_item_id where wok.user_id=:userId", nativeQuery = true)
+			+ "wok.attached_image, wok.parent_comment_id, wok.approval , item.name as work_item_name, item.currency_id,item.number, "
+			+ "CASE WHEN wok.approval = 1 THEN 'すごいね' WHEN wok.approval = 2 THEN 'いいね' WHEN wok.approval = 3 THEN 'がんばりましょう' ELSE 'だいじょうぶか？' END AS approval_message "
+			+ "FROM spajam.t_work wok INNER JOIN spajam.m_work_item item ON item.work_item_id = wok.work_item_id "
+			+ "WHERE wok.user_id=:userId", nativeQuery = true)
 	List<WorkDto> getWorks(@Param("userId") Integer userId);
 }
