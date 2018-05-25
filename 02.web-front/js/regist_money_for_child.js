d1c1ecae1a0013044cce295069f46d747c0c416f
yuta_koga <yutakoga1002@gmail.com>
2018-05-25 20:41:55 +0900

    20180525 koga fix2

--- 02.web-front/js/regist_money_for_child.js
+++ 02.web-front/js/regist_money_for_child.js
@@ -1,4 +1,4 @@
-var isMock = true;
+var isMock = false;
 var jsonData;
 var mockJson = {
     workItemList : [
@@ -162,7 +162,7 @@ function getTime() {
     var today = new Date();
     var time = today.getFullYear() + "/" +
             ('0' + (today.getMonth() + 1)).slice(-2) + "/" +
-            ('0' + today.getDate()).slice(-2) + "/ " +
+            ('0' + today.getDate()).slice(-2) + " " +
             ('0' + today.getHours()).slice(-2) + ":" +
             ('0' + today.getMinutes()).slice(-2) + ":" +
             ('0' + today.getSeconds()).slice(-2);
