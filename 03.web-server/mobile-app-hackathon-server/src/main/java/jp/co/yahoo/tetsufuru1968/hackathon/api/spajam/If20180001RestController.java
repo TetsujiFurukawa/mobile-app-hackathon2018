49ecd15f0709811749a7a77ff1ab5befb201c80f
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2018-05-25 21:38:38 +0900

    作成中

--- 03.web-server/mobile-app-hackathon-server/src/main/java/jp/co/yahoo/tetsufuru1968/hackathon/api/spajam/If20180001RestController.java
+++ 03.web-server/mobile-app-hackathon-server/src/main/java/jp/co/yahoo/tetsufuru1968/hackathon/api/spajam/If20180001RestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,7 @@ public class If20180001RestController {
 	If2018001Service if2018001Service;
 
 	@CrossOrigin
+	@PostMapping
 	@GetMapping
 	public WorkItemListDto getWorkItems() {
 		List<WorkItem> workItems = if2018001Service.getWorkItems();
--- 03.web-server/mobile-app-hackathon-server/src/main/java/jp/co/yahoo/tetsufuru1968/hackathon/api/spajam/If20180002RestController.java
+++ 03.web-server/mobile-app-hackathon-server/src/main/java/jp/co/yahoo/tetsufuru1968/hackathon/api/spajam/If20180002RestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,7 @@ public class If20180002RestController {
 
 	@CrossOrigin
 	@PostMapping
+	@GetMapping
 	public WorkListDto getWorks(@RequestBody WorkListSearchConditionDto searchCondition) {
 
 		// 親かどうか
